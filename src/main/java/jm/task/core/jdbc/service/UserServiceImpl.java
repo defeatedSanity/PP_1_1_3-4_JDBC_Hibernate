1614e4addcadd6a99d64f188a8e9b4d1a3038932
defeatedSanity <rome.kuznets@gmail.com>
2023-08-18 17:58:19 +0300

    initial commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,7 +4,7 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-public class UserServiceImpl implements UserServic {
+public class UserServiceImpl implements UserService {
     public void createUsersTable() {
 
     }
