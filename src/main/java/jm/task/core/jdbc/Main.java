0e5db9af0ab1247289d13904863503f7dc06ab28
defeatedSanity <rome.kuznets@gmail.com>
2023-08-18 17:57:57 +0300

    initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,24 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String userName = "root";
+        String password ="52826552";
+        String connectionUrl="jdbc:mysql://localhost:3306/users";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("create table Users (" +
+                    " id bigint not null," +
+                    " name VARCHAR(30)," +
+                    " surname varchar(30)," +
+                    " primary key (id))");
+        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,7 +4,7 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserServic {
     public void createUsersTable() {
 
     }
