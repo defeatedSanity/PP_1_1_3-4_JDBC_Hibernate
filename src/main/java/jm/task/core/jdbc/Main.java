95cda10e3e417ad2bbc448817d79e58af7014135
defeatedSanity <rome.kuznets@gmail.com>
2023-08-21 16:31:17 +0300

    end_hibernate_1.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,21 +4,18 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
+
         userService.createUsersTable();
 
         userService.saveUser("Aboba", "Ivanov", (byte) 20);
         userService.saveUser("Alex", "Zhmyh", (byte) 25);
         userService.saveUser("Valera", "Albertovich", (byte) 54);
         userService.saveUser("Denis", "Petrov", (byte) 33);
+
         System.out.println(userService.getAllUsers());
-        userService.removeUserById(2);
-        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
 
         userService.dropUsersTable();
-
-//        System.out.println(userService.getAllUsers());
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
-
     }
 }
